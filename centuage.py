3a625161ac933bb6550c3c43af5f0ff54a1cdcfb
sugheshc <37065627+sugheshc@users.noreply.github.com>
2021-01-25 13:03:29 +0530

    Add files via upload

---
+++ centuage.py
@@ -0,0 +1,10 @@
+#yob=1995
+#cyer=2021
+#26+74
+
+yob=input("enter the date of birth")
+dob=input("enter the current year")
+age=int(dob)-int(yob)
+print(age,"is your age")
+value=100-int(age)
+print(value,"years are required to complete 100 yrs")
---
+++ intro.py
@@ -0,0 +1,5 @@
+age=int(input("enter your age"))
+if age>=18:
+    print("you are eligible for voting")
+else:
+    ("you are not eligible for voting")
